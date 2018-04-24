0349f556154baf56ef43d44ff54b593fe459c7e6
agordo25 <34726753+agordo25@users.noreply.github.com>
2018-04-24 14:18:35 -0400

    Add files via upload

---
+++ DoNow.py
@@ -0,0 +1,13 @@
+#create a function that finds the maximum value in a list.
+#Once it finds the value, display it.
+#testScores=(77.7, 88.3, 99, 81.5, 86, 88, 78, 67, 95)
+
+def findMaxvaltestScores):
+    print("Original values: ", testScores)):
+    maxVal = 0
+    for i in range(0, len(testScores)):
+        if testScores{i} > maxVal:
+            maxVal = testScores{i}
+        print("the maximum value in the list is: ", maxVal)
+
+print(findMaxValue(testScores))
